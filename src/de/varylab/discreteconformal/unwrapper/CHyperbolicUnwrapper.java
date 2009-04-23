4af3db05b8c573f8bf0fa8691cbfede99ecc0ad8
Stefan Sechelmann <sechel@math.tu-berlin.de>
2009-04-23 15:06:37 +0000

    git-svn-id: http://fs:8000/svn/DDG@1895 986badc2-2932-0410-96f0-8e0caa59e138

--- src/de/varylab/discreteconformal/unwrapper/CHyperbolicUnwrapper.java
+++ src/de/varylab/discreteconformal/unwrapper/CHyperbolicUnwrapper.java
@@ -15,7 +15,6 @@ import de.varylab.mtjoptimization.stepcontrol.ArmijoStepController;
 public class CHyperbolicUnwrapper implements Unwrapper{
 
 	
-	
 	public Vector unwrap(CoHDS surface, int numCones, boolean quantizeCones) throws Exception {
 		surface.prepareInvariantDataHyperbolic();
 		CHyperbolicOptimizable opt = new CHyperbolicOptimizable(surface);
