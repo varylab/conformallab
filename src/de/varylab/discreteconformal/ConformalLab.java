52bccbec19280daf474dd7430471a025f3517dfd
Stefan Sechelmann <sechel@math.tu-berlin.de>
2009-05-08 00:11:08 +0000

    git-svn-id: http://fs:8000/svn/DDG@1951 986badc2-2932-0410-96f0-8e0caa59e138

--- src/de/varylab/discreteconformal/ConformalLab.java
+++ src/de/varylab/discreteconformal/ConformalLab.java
@@ -1,10 +1,8 @@
 package de.varylab.discreteconformal;
 
-import de.jreality.plugin.ContentViewer;
-import de.jreality.plugin.view.ContentLoader;
+import de.jreality.plugin.JRViewer;
 import de.jreality.plugin.view.InfoOverlayPlugin;
 import de.jreality.plugin.view.ViewerKeyListenerPlugin;
-import de.jreality.plugin.view.ZoomTool;
 import de.jtem.halfedge.plugin.HalfedgeConnectorPlugin;
 import de.jtem.halfedge.plugin.HalfedgeDebuggerPlugin;
 import de.jtem.halfedge.plugin.HalfedgeToolBar;
@@ -18,15 +16,14 @@ import de.varylab.discreteconformal.plugin.DiscreteConformalPlugin;
 public class ConformalLab {
 
 	public static void main(String[] args) {
-		ContentViewer viewer = new ContentViewer();
-		viewer.registerPlugin(new ContentLoader());
-		viewer.registerPlugin(new ZoomTool());
+		JRViewer viewer = JRViewer.createViewer();
+		viewer.setPropertiesFile("ConformalLab.jrw");
+		viewer.registerPlugin(new DiscreteConformalPlugin());
 		viewer.registerPlugin(new HalfedgeConnectorPlugin());
 		viewer.registerPlugin(new HalfedgeDebuggerPlugin<CoVertex, CoEdge, CoFace>());
 		viewer.registerPlugin(new HalfedgeToolBar());
 		viewer.registerPlugin(new CatmullClarkPlugin());
 		viewer.registerPlugin(new TriangulatePlugin());
-		viewer.registerPlugin(new DiscreteConformalPlugin());
 		viewer.registerPlugin(new ViewerKeyListenerPlugin());
 		viewer.registerPlugin(new InfoOverlayPlugin());
 		viewer.startup();
