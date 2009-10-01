77995779ec873a2d3136fcc0e0eb5456ce7aa15a
Stefan Sechelmann <sechel@math.tu-berlin.de>
2009-10-01 11:46:14 +0000

    git-svn-id: http://fs:8000/svn/DDG@2249 986badc2-2932-0410-96f0-8e0caa59e138

--- src/de/varylab/discreteconformal/plugin/SurfaceRemeshingPlugin.java
+++ src/de/varylab/discreteconformal/plugin/SurfaceRemeshingPlugin.java
@@ -12,13 +12,21 @@ import java.awt.event.ActionListener;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.Vector;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingUtilities;
 
+import de.jreality.math.Matrix;
 import de.jreality.plugin.basic.View;
+import de.jreality.plugin.content.ContentAppearance;
 import de.jreality.plugin.experimental.ManagedContent;
+import de.jreality.ui.AppearanceInspector;
 import de.jtem.halfedge.util.HalfEdgeUtils;
 import de.jtem.halfedgetools.jreality.adapter.Adapter;
 import de.jtem.halfedgetools.plugin.HalfedgeConnectorPlugin;
@@ -37,11 +45,21 @@ import de.varylab.discreteconformal.heds.bsp.KdTree;
 
 public class SurfaceRemeshingPlugin extends ShrinkPanelPlugin implements ActionListener {
 
-	public static abstract class MeshPattern {
-		  
-
-		
+	private enum Pattern {
+		Quads,
+		Hexagons;
 	}
+	
+	private ContentAppearance
+		contentAppearance = null;
+	private JComboBox
+		patternCombo = new JComboBox(Pattern.values());
+	private SpinnerNumberModel
+		lookUpHeuristicModel = new SpinnerNumberModel(3, 1, 100, 1),
+		resolutionModel = new SpinnerNumberModel(50, 1, 10000000, 1);
+	private JSpinner
+		lookUpHeuristicSpinner = new JSpinner(lookUpHeuristicModel),
+		resolutionSpinner = new JSpinner(resolutionModel);
 	
 	// plug-in connection
 	private ManagedContent
@@ -60,10 +78,20 @@ public class SurfaceRemeshingPlugin extends ShrinkPanelPlugin implements ActionL
 		gbc1.insets = new Insets(2, 2, 2, 2);
 		gbc1.gridwidth = GridBagConstraints.RELATIVE;
 		gbc1.weightx = 0.0;
+		gbc1.fill = GridBagConstraints.BOTH;
 		gbc2.insets = new Insets(2, 2, 2, 2);
 		gbc2.gridwidth = GridBagConstraints.REMAINDER;
 		gbc2.weightx = 1.0;
-		shrinkPanel.add(meshingButton);
+		gbc2.fill = GridBagConstraints.BOTH;
+
+		shrinkPanel.add(new JLabel("Pattern"), gbc1);
+		shrinkPanel.add(patternCombo, gbc2);
+		shrinkPanel.add(new JLabel("Lookup Heuristic"), gbc1);
+		shrinkPanel.add(lookUpHeuristicSpinner, gbc2);
+		shrinkPanel.add(new JLabel("Resolution"), gbc1);
+		shrinkPanel.add(resolutionSpinner, gbc2);
+		
+		shrinkPanel.add(meshingButton, gbc2);
 
 		meshingButton.addActionListener(this);
 	}
@@ -91,6 +119,14 @@ public class SurfaceRemeshingPlugin extends ShrinkPanelPlugin implements ActionL
 			return;
 		}
 		
+		AppearanceInspector ai = contentAppearance.getAppearanceInspector();
+		Matrix texMatrix = ai.getTextureMatrix();
+		Matrix texInvMatrix = texMatrix.getInverse();
+		
+		for (CoVertex v : hds.getVertices()) {
+			v.setTextureCoord(transformTexCoord(v.getTextureCoord(), texMatrix));
+		}
+		
 		double minX = hds.getVertex(0).getTextureCoord().x();
 		double maxX = hds.getVertex(0).getTextureCoord().x();
 		double minY = hds.getVertex(0).getTextureCoord().y();
@@ -109,64 +145,184 @@ public class SurfaceRemeshingPlugin extends ShrinkPanelPlugin implements ActionL
 				maxY = v.getTextureCoord().y();
 			}			
 		}
-		System.out.println("x[" + minX + ":" + maxX + "], y[" + minY + ":" + maxY + "]");
+
 		double xSpan = maxX - minX;
 		double ySpan = maxY - minY;
-		int xRes = 100;
-		int yRes = 100;
-		double xStep = xSpan / xRes;
-		double yStep = ySpan / yRes;
+
 		CoHDS r = new CoHDS();
-		for (int i = 0; i < xRes; i++) {
-			for (int j = 0; j < yRes; j++) {
-				CoVertex v = r.addNewVertex();
-				double xPos = minX + i * xStep;
-				double yPos = minY + j * yStep;
-				Point pos = new Point(xPos,yPos,0);
-				v.setPosition(pos);
+		// create pattern
+		switch (getPattern()) {
+		case Quads:
+			int xRes = (int)Math.ceil(xSpan * 2);
+			double xStep = 0.5;
+			int yRes = (int)Math.ceil(ySpan * 2); 
+			double yStep = 0.5;
+			for (int i = 0; i < xRes; i++) {
+				for (int j = 0; j < yRes; j++) {
+					CoVertex v = r.addNewVertex();
+					double xPos = minX + i * xStep;
+					double yPos = minY + j * yStep;
+					Point pos = new Point(xPos, yPos, 1);
+					v.setPosition(pos);
+					Point tex = new Point(xPos, yPos, 1);
+					v.setTextureCoord(transformTexCoord(tex, texInvMatrix));
+				}
+			}
+			for (int i = 0; i < xRes - 1; i++) {
+				for (int j = 0; j < yRes - 1; j++) {
+					CoVertex v1 = r.getVertex(i*yRes + j); 
+					CoVertex v2 = r.getVertex((i+1)*yRes + j); 
+					CoVertex v3 = r.getVertex((i+1)*yRes + j + 1); 
+					CoVertex v4 = r.getVertex(i*yRes + j + 1); 
+					HalfEdgeUtils.constructFaceByVertices(r, v1, v2, v3, v4);
+				}
+			}
+			break;
+		case Hexagons:
+			//TODO: Fix pattern
+			xRes = (int)Math.ceil(xSpan) * 2;
+			xStep = xSpan / xRes;
+			double radius = xStep * 0.75;
+			double h = Math.sqrt(radius*radius - radius*radius/4);
+			double patternAspect = (3*radius)/(2*h);
+			yRes = (int)Math.ceil(ySpan / h / patternAspect);
+			
+			double offsetX = 3*radius/4;
+			double offsetY = h;
+			
+			for (int i = 0; i < xRes; i++) {
+				for (int j = 0; j < yRes; j++) {
+					CoVertex v = r.addNewVertex();
+					double xPos = minX + i * xStep + offsetX; 
+					double yPos = minY + j * h * patternAspect + offsetY;
+					double move = (j % 2 == 0) ? 1 : -1;
+					move *= (i % 2 == 0) ? 1 : -1;
+					xPos += move*radius/4;
+					Point pos = new Point(xPos, yPos, 1);
+					v.setPosition(pos);
+					Point tex = new Point(xPos, yPos, 1);
+					v.setTextureCoord(transformTexCoord(tex, texInvMatrix));
+				}
+			}
+			
+			for (int i = 0; i < xRes - 1; i++) {
+				for (int j = 0; j < yRes - 2; j+=2) {
+					int colStep = i % 2 == 0 ? 0 : 1;
+					if (j + 2 + colStep >= yRes) {
+						continue;
+					}
+					CoVertex v1 = r.getVertex(i*yRes + j + colStep); 
+					CoVertex v2 = r.getVertex((i+1)*yRes + j + colStep); 
+					CoVertex v3 = r.getVertex((i+1)*yRes + j + 1 + colStep); 
+					CoVertex v4 = r.getVertex((i+1)*yRes + j + 2 + colStep); 
+					CoVertex v5 = r.getVertex(i*yRes + j + 2 + colStep); 
+					CoVertex v6 = r.getVertex(i*yRes + j + 1 + colStep); 
+					HalfEdgeUtils.constructFaceByVertices(r, v1, v2, v3, v4, v5, v6);
+				}
 			}
 		}
-		for (int i = 0; i < xRes - 1; i++) {
-			for (int j = 0; j < yRes - 1; j++) {
-				CoVertex v1 = r.getVertex(i*yRes + j); 
-				CoVertex v2 = r.getVertex((i+1)*yRes + j); 
-				CoVertex v3 = r.getVertex((i+1)*yRes + j + 1); 
-				CoVertex v4 = r.getVertex(i*yRes + j + 1); 
-				HalfEdgeUtils.constructFaceByVertices(r, v1, v2, v3, v4);
-			}
-		}
+		
+		
+		
 		PositionTexcoord[] vArr = new PositionTexcoord[hds.numVertices()];
 		int index = 0; 
 		for (CoVertex v : hds.getVertices()) {
 			PositionTexcoord pt = new PositionTexcoord(v.getTextureCoord(), v.getPosition(), v);
 			vArr[index++] = pt;
 		}
+		BaryFace[] bfArr = new BaryFace[hds.numFaces()];
+		index = 0;
+		for (CoFace f : hds.getFaces()) {
+			bfArr[index++] = new BaryFace(f);
+		}
 		
+		int lookUp = lookUpHeuristicModel.getNumber().intValue();
 		KdTree<PositionTexcoord> kdTree = new KdTree<PositionTexcoord>(vArr, 20, false);
+//		KdTree<BaryFace> kdTreeBary = new KdTree<BaryFace>(bfArr, 20, false);
 		Set<CoVertex> cutted = new HashSet<CoVertex>(r.getVertices());
 		for (CoVertex v : r.getVertices()) {
-			for (PositionTexcoord ref : kdTree.collectKNearest(v, 15)) {
-				Point patternPoint = v.getPosition();
-				CoVertex refV = ref.getVertex();
-				v.setTextureCoord(refV.getTextureCoord());
-				for (CoFace f : HalfEdgeUtils.facesIncidentWithVertex(refV)) {
-					List<CoVertex> b = HalfEdgeUtils.boundaryVertices(f);
-					Triangle tTex = new Triangle(b.get(0).getTextureCoord(), b.get(1).getTextureCoord(), b.get(2).getTextureCoord());
-					Triangle tPos = new Triangle(b.get(0).getPosition(), b.get(1).getPosition(), b.get(2).getPosition());
-					if (isInTriangle(patternPoint, tTex, 0)) {
-						Point bary = getBarycentic(patternPoint, tTex);
-						Point newPos = getCoordinate(bary, tPos);
-						v.setPosition(newPos);
-						cutted.remove(v);
-						break;
-					}
+			Point patternPoint = v.getPosition();
+			Vector<PositionTexcoord> posTexVec = kdTree.collectKNearest(v, lookUp);
+			Set<CoFace> checkFaces = new HashSet<CoFace>();
+			for (PositionTexcoord posTex : posTexVec) {
+				checkFaces.addAll(HalfEdgeUtils.facesIncidentWithVertex(posTex.getVertex()));
+			}
+			for (CoFace f : checkFaces) {
+				List<CoVertex> b = HalfEdgeUtils.boundaryVertices(f);
+				Triangle tTex = new Triangle(b.get(0).getTextureCoord(), b.get(1).getTextureCoord(), b.get(2).getTextureCoord());
+				Triangle tPos = new Triangle(b.get(0).getPosition(), b.get(1).getPosition(), b.get(2).getPosition());
+				if (isInTriangle(patternPoint, tTex, 0)) {
+					Point bary = getBarycentic(patternPoint, tTex);
+					Point newPos = getCoordinate(bary, tPos);
+					v.setPosition(newPos);
+					cutted.remove(v);
+					break;
 				}
 			}
 		}
 		
+//		
+//		Set<CoVertex> overlap = new HashSet<CoVertex>();
+//		for (CoVertex v : r.getVertices()) {
+//			if (cutted.contains(v)) {
+//				continue;
+//			}
+//			List<CoVertex> incident = HalfEdgeUtilsExtra.getVertexStar(v);
+//			for (CoVertex iv : incident) {
+//				if (cutted.contains(iv)) {
+//					overlap.add(iv);
+//					break;
+//				}
+//			}
+//		}
+//		cutted.removeAll(overlap);
+//		for (CoVertex v : overlap) {
+//			BaryFace bf = kdTreeBary.collectKNearest(v, 1).firstElement();
+//			CoFace f = bf.getFace();
+//			List<CoVertex> b = HalfEdgeUtils.boundaryVertices(f);
+//			Triangle tTex = new Triangle(b.get(0).getTextureCoord(), b.get(1).getTextureCoord(), b.get(2).getTextureCoord());
+//			Triangle tPos = new Triangle(b.get(0).getPosition(), b.get(1).getPosition(), b.get(2).getPosition());
+//			Point bary = getBarycentic(v.getPosition(), tTex);
+//			Point newPos = getCoordinate(bary, tPos);
+//			v.setPosition(newPos);
+//		}
 		
-		hcp.updateHalfedgeContent(r, true, new PositionAdapter());
+		
+		Set<CoFace> deleteFaces = new HashSet<CoFace>();
+		Set<CoEdge> deleteEdges = new HashSet<CoEdge>();
+		for (CoVertex v : cutted) {
+			for (CoEdge e : HalfEdgeUtils.incomingEdges(v)) {
+				deleteEdges.add(e);
+				deleteEdges.add(e.getOppositeEdge());
+			}
+			deleteFaces.addAll(HalfEdgeUtils.facesIncidentWithVertex(v));
+		}
+		for (CoVertex v : cutted) {
+			r.removeVertex(v);
+		}
+		for (CoEdge e : deleteEdges) {
+			r.removeEdge(e);
+		}
+		for (CoFace f : deleteFaces) {
+			r.removeFace(f);
+		}
+		
+		
+		hcp.updateHalfedgeContent(r, true, new PositionAdapter(), new TexCoordAdapter(true));
 	}
+	
+	
+	
+	private Point transformTexCoord(Point tex, Matrix M) {
+		double[] tmp = {0,0,0,1};
+		tmp[0] = tex.z() != 0 ? tex.x() / tex.z() : tex.x();
+		tmp[1] = tex.z() != 0 ? tex.y() / tex.z() : tex.y();
+		tmp[2] = 0;
+		tmp[3] = tex.z() != 0 ? tex.z() : 1;
+		double[] texCoord = M.multiplyVector(tmp);
+		return new Point(texCoord[0] / texCoord[3], texCoord[1] / texCoord[3], 1.0);
+	}
+	
 	
 	
 	private boolean isInTriangle(Point p , Triangle t, double tol) {
@@ -205,6 +361,38 @@ public class SurfaceRemeshingPlugin extends ShrinkPanelPlugin implements ActionL
 		return r;
 	}
 	
+	
+	
+	
+	public class BaryFace implements HasPosition {
+		
+		private Point
+			bary = null;
+		private CoFace
+			face = null;
+		
+		public BaryFace(CoFace f) {
+			this.face = f;
+			List<CoVertex> b = HalfEdgeUtils.boundaryVertices(f);
+			Triangle t = new Triangle(b.get(0).getTextureCoord(), b.get(1).getTextureCoord(), b.get(2).getTextureCoord());
+			bary = t.getBaryCenter();
+		}
+		
+		
+		@Override
+		public Point getPosition() {
+			return bary;
+		}
+		@Override
+		public void setPosition(Point p) {
+			bary.set(p.get());
+		}
+
+		public CoFace getFace() {
+			return face;
+		}
+		
+	}
 	
 	
 	
@@ -252,14 +440,20 @@ public class SurfaceRemeshingPlugin extends ShrinkPanelPlugin implements ActionL
 	
 	
 	
+	private Pattern getPattern() {
+		return (Pattern)patternCombo.getSelectedItem();
+	}
+	
+	
 	
 	
 	@SuppressWarnings("unchecked")
-	@Override
+	@Override 
 	public void install(Controller c) throws Exception {
 		super.install(c);
 		hcp = c.getPlugin(HalfedgeConnectorPlugin.class);
 		managedContent = c.getPlugin(ManagedContent.class);
+		contentAppearance = c.getPlugin(ContentAppearance.class);
 	}
 	
 	
@@ -267,8 +461,24 @@ public class SurfaceRemeshingPlugin extends ShrinkPanelPlugin implements ActionL
 	public void uninstall(Controller c) throws Exception {
 		super.uninstall(c);
 		managedContent.removeAll(getClass());
+	} 
+	
+	
+	@Override
+	public void storeStates(Controller c) throws Exception {
+		super.storeStates(c);
+		c.storeProperty(getClass(), "resolution", resolutionModel.getNumber().intValue());
+		c.storeProperty(getClass(), "lookUpHeuristic", lookUpHeuristicModel.getNumber().intValue());
+		c.storeProperty(getClass(), "pattern", patternCombo.getSelectedIndex());
 	}
 	
+	@Override
+	public void restoreStates(Controller c) throws Exception {
+		super.restoreStates(c);
+		resolutionModel.setValue(c.getProperty(getClass(), "resolution", resolutionModel.getNumber().intValue()));
+		lookUpHeuristicModel.setValue(c.getProperty(getClass(), "lookUpHeuristic", lookUpHeuristicModel.getNumber().intValue()));
+		patternCombo.setSelectedIndex(c.getProperty(getClass(), "pattern", 0));
+	}
 	
 	@Override
 	public Class<? extends SideContainerPerspective> getPerspectivePluginClass() {
