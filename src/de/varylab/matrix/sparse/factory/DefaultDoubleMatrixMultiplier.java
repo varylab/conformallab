c298d25593d2666927b46a712e0085d8498e4651
Felix Knöppel <knoeppel@math.tu-berlin.de>
2011-02-16 20:01:29 +0000

    no need anymore
    git-svn-id: http://fs:8000/svn/DDG@3298 986badc2-2932-0410-96f0-8e0caa59e138

--- src/de/varylab/matrix/sparse/factory/DefaultDoubleMatrixMultiplier.java
+++
@@ -1,8 +0,0 @@
-package de.varylab.matrix.sparse.factory;
-
-public class DefaultDoubleMatrixMultiplier extends ScalarDoubleMatrixMultilier {
-
-	public DefaultDoubleMatrixMultiplier() {
-		super();
-	}
-}
