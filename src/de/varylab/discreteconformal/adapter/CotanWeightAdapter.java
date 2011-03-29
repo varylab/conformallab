dbe6c94eb8639343337fd68cc5563e025c17ec8e
Felix Knöppel <knoeppel@math.tu-berlin.de>
2011-03-29 19:56:04 +0000

    git-svn-id: http://fs:8000/svn/DDG@3363 986badc2-2932-0410-96f0-8e0caa59e138

---
+++ src/de/varylab/discreteconformal/adapter/CotanWeightAdapter.java
@@ -0,0 +1,63 @@
+package de.varylab.discreteconformal.adapter;
+
+import de.jtem.halfedge.Edge;
+import de.jtem.halfedge.Face;
+import de.jtem.halfedge.Node;
+import de.jtem.halfedge.Vertex;
+import de.jtem.halfedgetools.adapter.AbstractAdapter;
+import de.jtem.halfedgetools.adapter.AdapterSet;
+import de.jtem.halfedgetools.adapter.type.Length;
+import de.jtem.halfedgetools.adapter.type.Weight;
+
+@Weight
+public class CotanWeightAdapter extends AbstractAdapter<Double> {
+
+	public CotanWeightAdapter() {
+		super(Double.class, true, false);
+	}
+
+	public <
+		V extends Vertex<V, E, F>,
+		E extends Edge<V, E, F>,
+		F extends Face<V, E, F>
+	> Double getE(E e, AdapterSet adapters) {
+		if (!adapters.contains(Length.class, e.getClass(), Double.class)) {
+			throw new RuntimeException(
+					"Need adapter for length of edges to calculate cotan weights.");
+		}
+
+		double leftcotanalpha = getLeftWeight(e, adapters);
+		double rightcotanalpha = getLeftWeight(e.getOppositeEdge(), adapters);
+
+		return .5*(leftcotanalpha+rightcotanalpha);
+	}
+
+	private <
+		V extends Vertex<V, E, F>,
+		E extends Edge<V, E, F>,
+		F extends Face<V, E, F>
+	> double getLeftWeight(E e, AdapterSet adapters) {
+	
+		double a = adapters.get(Length.class, e, Double.class);
+		double b = adapters
+				.get(Length.class, e.getNextEdge(), Double.class);
+		double c = adapters.get(Length.class,
+				e.getNextEdge().getNextEdge(), Double.class);
+
+		if (!e.getNextEdge().getNextEdge().getNextEdge().equals(e)) {
+			throw new RuntimeException("Face is not a triangle.");
+		}
+
+		double cosalpha = (b * b + c * c - a * a) / (2 * b * c);
+		double cotanalpha = cosalpha / (Math.sqrt(1 - cosalpha * cosalpha));
+		return cotanalpha;
+	};
+
+	@Override
+	public <
+		N extends Node<?, ?, ?>
+	> boolean canAccept(Class<N> nodeClass) {
+		return Edge.class.isAssignableFrom(nodeClass);
+	}
+
+}
