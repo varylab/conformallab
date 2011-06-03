a535aa9807a8edb39a3f01ceac4d960c0cf40dc1
Stefan Sechelmann <sechel@math.tu-berlin.de>
2011-06-03 12:01:20 +0000

    git-svn-id: http://fs:8000/svn/DDG@3472 986badc2-2932-0410-96f0-8e0caa59e138

--- src-test/de/varylab/discreteconformal/heds/HyperbolicLayoutTest.java
+++ src-test/de/varylab/discreteconformal/heds/HyperbolicLayoutTest.java
@@ -2,17 +2,9 @@ package de.varylab.discreteconformal.heds;
 
 import java.io.IOException;
 
-import no.uib.cipr.matrix.Vector;
-import no.uib.cipr.matrix.sparse.SparseVector;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import de.jreality.math.Pn;
 import de.jreality.plugin.JRViewer;
 import de.jreality.reader.ReaderOBJ;
 import de.jreality.scene.IndexedFaceSet;
@@ -24,9 +16,6 @@ import de.jtem.halfedgetools.adapter.AdapterSet;
 import de.jtem.halfedgetools.jreality.ConverterHeds2JR;
 import de.jtem.halfedgetools.jreality.ConverterJR2Heds;
 import de.varylab.discreteconformal.heds.adapter.CoPositionAdapter;
-import de.varylab.discreteconformal.heds.adapter.CoTexturePositionAdapter;
-import de.varylab.discreteconformal.unwrapper.HyperbolicLayout;
-import de.varylab.discreteconformal.util.UnwrapUtility;
 
 public class HyperbolicLayoutTest {
 
@@ -51,48 +40,27 @@ public class HyperbolicLayoutTest {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		AdapterSet a = AdapterSet.createGenericAdapters();
-		a.add(new CoPositionAdapter());
-		a.add(new CoPositionAdapter());
-		a.add(new CoTexturePositionAdapter());
-		UnwrapUtility.prepareInvariantDataHyperbolic(hds, a);
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		System.out.println("CLayoutTest.tearDownAfterClass()");
-	}
-
-	@Before
-	public void setUp() throws Exception {
-		System.out.println("CLayoutTest.setUp()");
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		System.out.println("CLayoutTest.tearDown()");
 	}
 
 	@Test
 	public void testDoLayout() {
-		System.out.println("CLayoutTest.testDoLayout()");
-		AdapterSet a = AdapterSet.createGenericAdapters();
-		a.add(new CoPositionAdapter());
-		a.add(new CoPositionAdapter());
-		a.add(new CoTexturePositionAdapter());
-		int n = UnwrapUtility.prepareInvariantDataHyperbolic(hds, a);
-		Vector u = new SparseVector(n);
-		HyperbolicLayout.doLayout(hds, hds.getVertex(0), u);
-		
-		for (CoEdge e : hds.getPositiveEdges()) {
-			double[] s = e.getStartVertex().P;
-			double[] t = e.getTargetVertex().P;
-			double[] st = e.getStartVertex().T;
-			double[] tt = e.getTargetVertex().T;
-			double l1 = Pn.distanceBetween(s, t, Pn.HYPERBOLIC);
-			double l2 = Pn.distanceBetween(st, tt, Pn.HYPERBOLIC);
-			Assert.assertEquals(l1, l2, 1E-3);
-		}
+		throw new RuntimeException("Not implemented correctly yet!");
+//		System.out.println("CLayoutTest.testDoLayout()");
+//		AdapterSet a = AdapterSet.createGenericAdapters();
+//		a.add(new CoPositionAdapter());
+//		a.add(new CoPositionAdapter());
+//		a.add(new CoTexturePositionAdapter());
+//		int n = UnwrapUtility.prepareInvariantDataHyperbolic(hds, a);
+//		Vector u = new SparseVector(n);
+//		HyperbolicLayout.doLayout(hds, hds.getVertex(0), u);
+//		
+//		for (CoEdge e : hds.getPositiveEdges()) {
+//			double[] st = e.getStartVertex().T;
+//			double[] tt = e.getTargetVertex().T;
+//			double l1 = HyperbolicLayout.getNewLength(e, u);
+//			double l2 = Pn.distanceBetween(st, tt, Pn.HYPERBOLIC);
+//			Assert.assertEquals(l1, l2, 1E-3);
+//		}
 	}
 
 	public static void main(String[] args) throws Exception{
