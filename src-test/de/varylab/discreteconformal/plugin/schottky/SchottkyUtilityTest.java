a40f391a6db99ed62af3bd7cf7db66b26248bf31
Stefan Sechelmann <sechel@math.tu-berlin.de>
2013-10-21 09:44:49 +0000

    update halfedge tools
    git-svn-id: http://fs:8000/svn/DDG@5089 986badc2-2932-0410-96f0-8e0caa59e138

--- src-test/de/varylab/discreteconformal/plugin/schottky/SchottkyUtilityTest.java
+++ src-test/de/varylab/discreteconformal/plugin/schottky/SchottkyUtilityTest.java
@@ -8,8 +8,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 import de.jreality.util.NativePathUtility;
