925eb3765367394699d9a34577e04742ba4231e6
Stefan Sechelmann <sechel@math.tu-berlin.de>
2014-06-04 15:42:26 +0200

    tests for the dilogarithm function work

--- src-test/de/varylab/discreteconformal/functional/ClausenTest.java
+++ src-test/de/varylab/discreteconformal/functional/ClausenTest.java
@@ -1,5 +1,6 @@
 package de.varylab.discreteconformal.functional;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import de.jtem.mfc.field.Complex;
@@ -8,9 +9,22 @@ public class ClausenTest {
 
 	@Test
 	public void testImLi2() throws Exception {
+		// compare with results from mathematica
 		Complex z = new Complex(0.5, 0.5);
 		double r = Clausen.ImLi2(z);
-		System.out.println(r);
+		Assert.assertEquals(0.643767332889268748742017, r, 1E-15);
+		
+		z = new Complex(1.234, -3.554);
+		r = Clausen.ImLi2(z);
+		Assert.assertEquals(-2.784709023190200241929031, r, 1E-15);	
+		
+		z = new Complex(-4.254, 2.965);
+		r = Clausen.ImLi2(z);
+		Assert.assertEquals(1.104092479314665907914598, r, 1E-15);
+		
+		z = new Complex(-2.264, -1.05);
+		r = Clausen.ImLi2(z);
+		Assert.assertEquals(-0.540683148040955780529547, r, 1E-15);
 	}
 	
 }
